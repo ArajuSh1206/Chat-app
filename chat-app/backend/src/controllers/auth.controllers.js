c66a3e8a6f45156413efeda85fb9a3c723285bc7
AriiSharma <ariisharma47@gmail.com>
2025-02-03 15:56:26 -0700

    added cloudinary for pfp UI.

--- chat-app/backend/src/controllers/auth.controllers.js
+++ chat-app/backend/src/controllers/auth.controllers.js
@@ -1,6 +1,8 @@
 import User from "../models/user.model.js";
 import { generateToken } from "../lib/utils.js";
+import { protectRoute } from "../middleware/auth.middleware.js";
 import bcrypt from "bcryptjs";
+import cloudinary from "../lib/cloudinary.js";
 
 export const signup = async (req, res) => {
     const { fullname, email, password } = req.body;
@@ -87,5 +89,25 @@ export const logout = (req, res) => {
     } catch (error){
         console.log("Error in logout controller", error.message);
         res.status(500).json({message: "Internal server error."}); 
+    }
+};
+
+export const updateProfile = async (req, res) => {
+    try {
+        const {profilePic} = req.body;
+        const userId = req.user._id;
+
+        if(!profilePic) {
+            return res.status(400).json({ message: "Profile pic is required" });
+        }
+        
+        const uploadResponse = await cloudinary.uploader.upload(profilePic)
+        const updatedUser = await user.findByIdAndUpdate(userId, {profilePic: uploadResponse.secure_url}, {new: true})
+
+        res.status(200).json(updatedUser)
+    
+    } catch (error) {
+        console.log("Error in update profile: ", error);
+        return res.status(500).json({ message: "Internal server error"});
     }
 };
--- chat-app/backend/src/index.js
+++ chat-app/backend/src/index.js
@@ -1,18 +1,21 @@
 import express from "express";
 import dotenv from "dotenv";
+import cookieParser from "cookie-parser";
 
 import authRoute from "./routes/auth.route.js";
 import { connectDB } from "./lib/db.js";
 
-dotenv.config(); // Added missing semicolon
+dotenv.config(); 
 
 const app = express();
 const PORT = process.env.PORT || 5173;
 
 app.use(express.json());
+app.use(cookieParser());
+
 app.use("/api/auth", authRoute);
 
-connectDB(); // Call this before starting the server
+connectDB(); 
 
 app.listen(PORT, () => {
     console.log("server is running on PORT:" + PORT);
