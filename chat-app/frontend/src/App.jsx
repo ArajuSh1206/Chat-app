576654f73dcded47afc19accd94be97d9a467f21
AriiSharma <ariisharma47@gmail.com>
2025-02-07 16:56:09 -0700

    Setting up the front-end, created pages. (ie. Signup, Login, profile, settings, home)

--- chat-app/frontend/src/App.jsx
+++ chat-app/frontend/src/App.jsx
@@ -1,8 +1,32 @@
 import React from 'react'
+import Navbar from './components/Navbar'
+import { Routes, Route} from "react-router-dom"
+
+import HomePage from "./pages/HomePage"
+import SignupPage from "./pages/SignupPage"
+import LoginPage from "./pages/LoginPage"
+import ProfilePage from "./pages/ProfilePage"
+import SettingsPage from "./pages/SettingsPage"
+
+
 
 const App = () => {
   return (
-    <div className = "text-pink-400"> hello</div>
+    <div>
+
+      <Navbar />
+
+      <Routes>
+        <Route path = "/" element = {<HomePage />} />
+        <Route path = "/signup" element = {<SignupPage />} />
+        <Route path = "/login" element = {<LoginPage />} />
+        <Route path = "/settings" element = {<SettingsPage />} />
+        <Route path = "/profile" element = {<ProfilePage />} />
+
+      </Routes>
+
+
+    </div>
   )
 }
 
